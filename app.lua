3a34a63a79504c0ead711fbcb4c864c6d39605ce
kmafeni04 <kuluislit@gmail.com>
2024-02-11 16:31:47 +0100

    Work on contact page
    Work on contact page and removing unused html and css in other views

--- app.lua
+++ app.lua
@@ -24,6 +24,7 @@ end)
 app:get("/contact", function()
 	return { render = "pages.contact" }
 end)
+
 -- components --
 
 app:get("/card1", function()
@@ -52,20 +53,20 @@ app:get("/dropdown1", function()
 	Header = "What is topic listing?"
 	Paragraph =
 	"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis"
-	return {render = "components.dropdown"}
+	return { render = "components.dropdown" }
 end)
 
 app:get("/dropdown2", function()
 	Header = "How to find a topic?"
 	Paragraph =
 	"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis"
-	return {render = "components.dropdown"}
+	return { render = "components.dropdown" }
 end)
 app:get("/dropdown3", function()
 	Header = "Does it need to be paid?"
 	Paragraph =
 	"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis"
-	return {render = "components.dropdown"}
+	return { render = "components.dropdown" }
 end)
 
 
